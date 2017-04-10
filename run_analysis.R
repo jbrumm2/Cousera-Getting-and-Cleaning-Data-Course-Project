5dc211e3d5b96fd8b77fa5265ed7bff958f6dac0
jbrumm2 <j.brumm@ymail.com>
2017-04-10 13:40:30 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+## Coursera Getting and Cleaning Data Course Project
+## Justine Brumm
+## 04/09/2017
+
+
+# Clean up workspace and load packages
+rm(list=ls())
+library(reshape2)
+
+# Set working directory to location where UCI HAR Dataset was unzipped
+setwd('/Users/Jus/Documents/Coursera data science/')
+
+# Load activity labels and features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract only data on mean and standard deviation
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub("-mean", "Mean", featuresWanted.names)
+featuresWanted.names = gsub("-std", "Std", featuresWanted.names)
+featuresWanted.names <- gsub("[-()]", "", featuresWanted.names)
+
+# Load the datasets
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+
+# Merge datasets and add labels
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
+
+# Turn activities and subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
+
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
